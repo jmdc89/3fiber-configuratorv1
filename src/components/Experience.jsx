76b3470243f304db288f4e920990db9fd3641281
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-09-18 10:19:48 -0600

    Update Experience.jsx
    stage

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,14 @@ import { OrbitControls } from "@react-three/drei";
 export const Experience = () => {
   return (
     <>
+      <Stage intensity={1.5} environment="city" shadows={{
+        type: "accumulative",
+        color: "#85ffbd",
+        colorBlend: 2,
+        opacity: 2,
+      }}
+        adjustCamera={2}
+      ></Stage>
       <OrbitControls />
       <mesh>
         <meshNormalMaterial />
