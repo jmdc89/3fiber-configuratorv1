0d3c02cdd717e0f54119f806ab3ea79ccf6a355b
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-09-18 11:19:01 -0600

    Update Table.jsx
    castShadow

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -10,13 +10,13 @@ export function Table(props) {
   const { nodes, materials } = useGLTF('/models/Table.gltf')
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.Plate.geometry} material={materials.Plate} />
-      <mesh geometry={nodes.Legs01Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
-      <mesh geometry={nodes.Legs01Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
-      <mesh geometry={nodes.Legs02Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
-      <mesh geometry={nodes.Legs02Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
-      <mesh geometry={nodes.Legs03Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
-      <mesh geometry={nodes.Legs03Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Plate.geometry} material={materials.Plate} />
+      <mesh castShadow geometry={nodes.Legs01Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Legs01Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Legs02Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Legs02Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Legs03Left.geometry} material={materials.Metal} position={[-1.5, 0, 0]} />
+      <mesh castShadow geometry={nodes.Legs03Right.geometry} material={materials.Metal} position={[1.5, 0, 0]} />
     </group>
   )
 }
